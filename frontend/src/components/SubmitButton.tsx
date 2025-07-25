6551cfb2fd84ba3740950e08dd8be74a6a766dff
ZiglaCity <ziglacity@gmail.com>
2025-07-25 18:37:42 +0000

    reduce submit button size

--- frontend/src/components/SubmitButton.tsx
+++ frontend/src/components/SubmitButton.tsx
@@ -13,17 +13,19 @@ const SubmitButton = ({onSubmit, isLoading, selected } : ButtonType) => {
   };
 
   return (
-    <button
-      onClick={handleSubmit}
-      disabled={!selected || isLoading}
-      className={`w-full py-2 rounded-md font-medium text-white px-4 bg-blue-600 hover:bg-blue-700 ${
-      isLoading || !selected
-          ? 'bg-dev-accent/50 cursor-not-allowed'
-          : 'bg-dev-accent hover:bg-dev-accent-muted'
-      }`}
-    >
-      {isLoading ? 'Analyzing...' : 'Analyze Code'}
-    </button>
+    <div className="">
+      <button
+        onClick={handleSubmit}
+        disabled={!selected || isLoading}
+        className={` py-2 rounded-md font-medium text-white px-4 bg-blue-600 hover:bg-blue-700 ${
+        isLoading || !selected
+            ? 'bg-dev-accent/50 cursor-not-allowed'
+            : 'bg-dev-accent hover:bg-dev-accent-muted'
+        }`}
+      >
+        {isLoading ? 'Analyzing...' : 'Analyze Code'}
+      </button>
+    </div>
   )
 }
 
