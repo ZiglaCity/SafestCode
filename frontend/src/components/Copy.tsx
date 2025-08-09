7f2ac72847b355efbf8a134f852dc2c73f6434a9
ZiglaCity <ziglacity@gmail.com>
2025-08-09 02:48:23 +0000

    Update: add style to copy  button

--- frontend/src/components/Copy.tsx
+++ frontend/src/components/Copy.tsx
@@ -4,13 +4,18 @@ interface Props {
   copyCode: () => void;
 }
 
-
-export default function Copy({copyCode} : Props){
+export default function Copy({ copyCode }: Props) {
   return (
     <div>
-      <button title="Copy" className="mx-3" onClick={copyCode}>
-        <CopyIcon />
+      <button
+        title="Copy"
+        onClick={copyCode}
+        className="mx-1 p-2 rounded-md bg-gray-100 hover:bg-gray-200 
+                   dark:bg-gray-800 dark:hover:bg-gray-700 
+                   transition-colors duration-200 shadow-sm border border-gray-300 dark:border-gray-700"
+      >
+        <CopyIcon className="w-5 h-5 text-gray-700 dark:text-gray-200" />
       </button>
     </div>
-  )
+  );
 }
